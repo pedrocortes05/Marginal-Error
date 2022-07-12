e8451cf7bbac33f1ef1d96f9ebc38bb6c14cc8a9
pedrocortes05 <pedrocortespro@gmail.com>
2022-07-12 15:13:46 -0500

    still figuring out what to put in the map table

--- db/migrate/20220712191147_create_maps.rb
+++ db/migrate/20220712191147_create_maps.rb
@@ -1,11 +1,10 @@
 class CreateMaps < ActiveRecord::Migration[7.0]
   def change
-    create_table :maps do |t|
+    create_table :maps, id: false do |t|
       t.integer :row
       t.integer :col
-      t.integer :value
-
-      t.timestamps
+      t.references :value, references: :players, foreign_key: { to_table: :players }
+      t.index [:row, :col], unique: true
     end
   end
 end
